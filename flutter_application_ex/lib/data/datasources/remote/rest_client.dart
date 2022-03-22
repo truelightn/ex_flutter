eee033b9ad424f1f179fa21ec4da8c7137a83252
truelightn <truelightn@gmail.com>
2022-03-22 18:20:18 +0900

    Retrofit client 추가

---
+++ flutter_application_ex/lib/data/datasources/remote/rest_client.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_application_ex/data/models/post.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'rest_client.g.dart';
+
+@RestApi(baseUrl: "https://my-json-server.typicode.com/truelightn/ex_flutter")
+abstract class RestClient {
+  factory RestClient(Dio dio, {String baseUrl}) = _RestClient;
+
+  @GET('/posts')
+  Future<Post> getPosts();
+}
