9fb4cde191a5b04d08f2fbd9a534ee6e93e92a63
truelightn <truelightn@gmail.com>
2022-03-23 14:47:22 +0900

    repository 수정 2

--- flutter_application_ex/lib/data/repositories/post_repositories.dart
+++ flutter_application_ex/lib/data/repositories/post_repositories.dart
@@ -2,12 +2,12 @@ import 'package:dio/dio.dart';
 import 'package:flutter_application_ex/data/datasources/remote/rest_client.dart';
 import 'package:flutter_application_ex/data/models/post.dart';
 
-class RankingRepositories {
+class PostRepositories {
   final RestClient _restClient;
 
   // const RankingRepositories(this._restClient);
 
-  RankingRepositories.create() : _restClient = RestClient(Dio());
+  PostRepositories.create() : _restClient = RestClient(Dio());
 
   Future<Post> getRankings() async {
     try {
