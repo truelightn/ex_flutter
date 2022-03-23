5ca34d3bcc5a0201523b7b280d9e139ba80f7240
truelightn <truelightn@gmail.com>
2022-03-23 14:16:23 +0900

    Repositories 생성

---
+++ flutter_application_ex/lib/data/repositories/post_repositories.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_application_ex/data/datasources/remote/rest_client.dart';
+import 'package:flutter_application_ex/data/models/post.dart';
+
+class RankingRepositories {
+  final RestClient _restClient;
+
+  const RankingRepositories(this._restClient);
+
+  RankingRepositories.create() : _restClient = RestClient(Dio());
+
+  Future<Post> getRankings() async {
+    try {
+      final result = await _restClient.getPosts();
+      return result;
+    } on DioError {
+      rethrow;
+    }
+  }
+}
